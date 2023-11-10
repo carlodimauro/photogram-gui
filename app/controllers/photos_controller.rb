b286523f6e4d120687a6e72f2a16d30a298e3b67
carlodimauro <146589712+carlodimauro@users.noreply.github.com>
2023-11-10 22:35:56 +0000

    Adds Photo#show

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,14 @@
+class PhotosController < ApplicationController
+
+  def index
+    @list_of_photos = Photo.all.order({:created_at => :desc})
+  render({:template => "phototemplates/index"})
+  end
+
+  def show
+    input_photo = params.fetch("photoidpath")
+    @currentphoto = Photo.where(:id => input_photo).first
+    render({:template => "phototemplates/show"})
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get("/users", {:controller=> "users", :action => "index"})
   get("/users/:useridpath", {:controller=> "users", :action => "show"})
 
+  get("/photos", {:controller=> "photos", :action => "index"})
+  get("/photos/:photoidpath", {:controller=> "photos", :action => "show"})
+
 end
